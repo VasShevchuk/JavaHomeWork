955f4dc69c21f7191711e21a85ed4a01f1140492
VasShevchuk <vasilij.shevchuk@privatbank.ua>
2021-10-14 23:56:21 +0300

    init commit

--- src/com/pb/shevchuk/hw2/Calculator.java
+++ src/com/pb/shevchuk/hw2/Calculator.java
@@ -1,5 +1,47 @@
 package com.pb.shevchuk.hw2;
 
+import javafx.util.converter.PercentageStringConverter;
+
+import java.util.Scanner;
+
 public class Calculator {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int c;
+        System.out.print("Введите operand1: ");
+        int operand1 = in.nextInt();
+        System.out.print("Введите operand2: ");
+        int operand2 = in.nextInt();
+        System.out.print("Введите действие ");
+        String v = in.next();
+        char sign = v.charAt(0);
+        switch (sign) {
+            case '*':
+                c= operand1*operand2;
+                System.out.println("результат"+c);
+                break;
+            case '/':
+                if (operand2==0) {
+                    System.out.print("деление на 0 запрещено");
+                break;
+                }
+        else{}
+
+                c= operand1/operand2;
+                System.out.println("результат"+c);
+                break;
+            case '+':
+                c= operand1+operand2;
+                System.out.println("Результат" +c);
+            case '-':
+                c= operand1-operand2;
+                System.out.println("результат" +c);
+                break;
+            default:
+                System.out.println("Неверный оператор");
+        }
+
+
+    }
 
 }
